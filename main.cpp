ca706fd8dd2d8b3dc7e9644f234e009e0b7fd3be
Matin <matin.akbari06ap@gmail.com>
2025-10-19 16:19:44 +0330

    test item-list class

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 
 
 #include "item.h"
+#include "item-list.h"
+#include "main.h"
 
 
 using namespace std;
 
 
 int main() {
-    // Item item1("Burger", 5.99, 0);
-    // Item item2("Coke", 1.99, 1);
-    // Item item3("Ice Cream", 2.99, 2);
+    Item item1("Burger", 5.99, 0);
+    Item item2("Coke", 1.99, 1);
+    Item item3("Ice Cream", 2.99, 2);
 
     // item1.set_next(&item2);
     // item2.set_next(&item3);
@@ -21,6 +23,23 @@ int main() {
     //     current->display();
     //     current = current->get_next();
     // }
+
+    // ItemList list;
+    // list.add_item("Burger", 5.99, 0);
+    // list.add_item("Coke", 1.99, 1);
+    // list.display_items();
+    // Item* found_item = list.find("Coke");
+    // if (found_item != nullptr) {
+    //     cout << "Found item: ";
+    //     found_item->display();
+    // } else {
+    //     cout << "Item not found." << endl;
+    // }
+    // list.remove("Burger");
+    // list.display_items();  
+    // list.add_item("Ice Cream", 2.99, 2);
+    // cout << list.get_size();
+    
     
 
 
