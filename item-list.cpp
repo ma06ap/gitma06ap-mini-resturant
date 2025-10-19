c796a761b09e54f732ca0006ec3b9909e620cbd6
Matin <matin.akbari06ap@gmail.com>
2025-10-19 13:10:39 +0330

    Define Item-list

---
+++ item-list.cpp
@@ -0,0 +1,53 @@
+#include "item-list.h"
+
+using namespace std;
+
+ItemList::ItemList() {
+    head = nullptr;
+    size = 0;
+}
+void ItemList::add_item(string name, float price, int type) {
+    Item* new_item = new Item(name, price, type);
+    new_item->set_next(head);
+    head = new_item;
+    size++;
+}
+void ItemList::display_items() {
+    Item* current = head;
+    while (current != nullptr) {
+        current->display();
+        current = current->get_next();
+    }
+}
+Item* ItemList::find(string name) {
+    Item* current = head;
+    while (current != nullptr) {
+        if (current->get_name() == name) {
+            return current;
+        }
+        cout << "Item not found: " << current->get_name() << endl;
+        current = current->get_next();
+    }
+    return nullptr;
+}
+void ItemList::remove(string name) {
+    Item* current = head;
+    Item* previous = nullptr;
+    while (current != nullptr) {
+        if (current->get_name() == name) {
+            if (previous == nullptr) {
+                head = current->get_next();
+            } else {
+                previous->set_next(current->get_next());
+            }
+            delete current;
+            size--;
+            return;
+        }
+        previous = current;
+        current = current->get_next();
+    }
+}
+int ItemList::get_size() {
+    return size;
+}
---
+++ item-list.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "item.h"
+
+using namespace std;
+
+class ItemList {
+private:
+    Item* head;
+    int size;
+
+public:
+    ItemList();
+
+    void add_item(string name, float price, int type);
+    void display_items();
+    Item* find(string name);
+    void remove(string name);
+    int get_size();
+
+
+
+
+
+
+
+};
