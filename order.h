da2e2e11042c17a631ba777650d991601d7ae204
Matin <matin.akbari06ap@gmail.com>
2025-10-19 17:18:00 +0330

    Define order class (just variables)

---
+++ order.h
@@ -0,0 +1,25 @@
+#pragma once 
+
+#include "student.h"
+#include "item-list.h"
+
+
+
+using namespace std;
+
+
+
+
+class Order {
+private: 
+    Student* customer;
+    ItemList* lorder;
+    Order* next;
+public: 
+
+
+
+
+
+};
+
