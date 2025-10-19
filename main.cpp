f5d4a9c8f799d09a444d27419c55ad09ba36f853
Matin <matin.akbari06ap@gmail.com>
2025-10-19 12:29:28 +0330

    Add main for test class

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+#include "item.h"
+
+
+using namespace std;
+
+
+int main() {
+    // Item item1("Burger", 5.99, 0);
+    // Item item2("Coke", 1.99, 1);
+    // Item item3("Ice Cream", 2.99, 2);
+
+    // item1.set_next(&item2);
+    // item2.set_next(&item3);
+
+    // Item* current = &item1;
+    // while (current != nullptr) {
+    //     current->display();
+    //     current = current->get_next();
+    // }
+    
+
+
+
+    return 0;
+}
