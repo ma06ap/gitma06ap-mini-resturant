d4190ac35316b38596f819b2644a75592df05f60
Matin <matin.akbari06ap@gmail.com>
2025-10-20 12:56:18 +0330

    Add Order status and id + search based on id

--- order.cpp
+++ order.cpp
@@ -9,16 +9,57 @@ Order::Order(Student* c,ItemList* il,Order* ne) {
     customer = c;
     lorder = il;
     next = ne;
-};
+    status = Pending;
+    id = 0; 
+}
 
 
 
 Student* Order::get_student () {
     return customer;
-};
+}
 ItemList* Order::get_Itemes () {
     return lorder;
-};
+}
 Order* Order::get_next () {
     return next;
-};
+}
+
+
+void Order::change_status(int in) {
+    //
+    cout << "enter change menu" << endl;
+    if (status == cancled) {
+        cout << "Order was canceled" << endl;
+        return;
+    }
+    else if (status == delivered) {
+        cout << "Order was delivered" << endl;
+        return;
+    }
+    else {
+        status = static_cast<OrderStatus>(in);
+        if (in == 1) {
+            cout << "Order in process" << endl;
+        } else if (in == 2) {
+            cout << "Order delivered" << endl;
+        }
+        else if (in == 3) {
+            cout << "Order canceled." << endl;
+        }
+    }
+}
+
+void Order::set_id (int i) {
+    id = i;
+    cout << "id setted :" << id;
+}
+
+int Order::get_id () {
+    return id;
+}
+void Order::display () {
+    this->customer->display();
+    this->lorder->display_items();
+    cout << id << endl;
+}
--- order.h
+++ order.h
@@ -7,7 +7,12 @@
 
 using namespace std;
 
-
+enum OrderStatus{
+    Pending,
+    Inprocess,
+    delivered,
+    cancled,
+};
 
 
 class Order {
@@ -15,13 +20,16 @@ private:
     Student* customer;
     ItemList* lorder;
     Order* next;
+    OrderStatus status;
+    int id;
 public: 
     Order(Student* c,ItemList* il,Order* ne);
     Student* get_student ();
     ItemList* get_Itemes ();
     Order* get_next ();
-
-
-
+    void change_status(int in);
+    void set_id (int i);
+    int get_id ();
+    void display ();
 };
 
